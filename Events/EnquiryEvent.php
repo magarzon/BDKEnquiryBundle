467f6f6c0a3de1be05bba10a6498810e81f8b032
magarzon <magarzon@singularfactory.com>
2013-02-25 14:20:41 +0000

    Argument type in event class methods

--- Events/EnquiryEvent.php
+++ Events/EnquiryEvent.php
@@ -3,35 +3,48 @@
 namespace Bodaclick\BDKEnquiryBundle\Events;
 
 use Symfony\Component\EventDispatcher\Event;
+use Bodaclick\BDKEnquiryBundle\Model\Enquiry;
 
+/**
+ * Event dispatched in Enquiry Service
+ */
 class EnquiryEvent extends Event
 {
+    /**
+     * Enquiry associated to the event.
+     *
+     * @var Enquiry
+     */
     protected $enquiry;
-    protected $stopAction;
 
-    public function __construct($enquiry)
+    /**
+     * Constructor
+     *
+     * @param Enquiry $enquiry
+     */
+    public function __construct(Enquiry $enquiry)
     {
         $this->enquiry = $enquiry;
     }
 
-    public function setEnquiry($enquiry)
+    /**
+     * Set the enquiry object associated to the event
+     *
+     * @param Enquiry $enquiry
+     * @return EnquiryEvent
+     */
+    public function setEnquiry(Enquiry $enquiry)
     {
         $this->enquiry = $enquiry;
         return $this;
     }
 
+    /**
+     * Get the enquiry object associated to the event
+     * @return Enquiry
+     */
     public function getEnquiry()
     {
         return $this->enquiry;
-    }
-
-    public function setStopAction($stopAction)
-    {
-        $this->stopAction = $stopAction;
-    }
-
-    public function getStopAction()
-    {
-        return $this->stopAction;
     }
 }
