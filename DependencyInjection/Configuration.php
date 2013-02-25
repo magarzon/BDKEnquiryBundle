1bdfe4794201eadf2510a138d241bfff3b7950fd
magarzon <magarzon@singularfactory.com>
2013-02-25 14:11:15 +0000

    Changes made to apply right coding styles

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -36,8 +36,9 @@ class Configuration implements ConfigurationInterface
             ->scalarNode('db_driver')
                 ->validate()
                 ->ifNotInArray($supportedDrivers)
-                    ->thenInvalid('The driver %s is not supported. Please choose one of '
-                                .json_encode($supportedDrivers))
+                    ->thenInvalid(
+                        'The driver %s is not supported. Please choose one of '.json_encode($supportedDrivers)
+                    )
                 ->end()
                 ->cannotBeOverwritten()
                 ->isRequired()
@@ -60,8 +61,10 @@ class Configuration implements ConfigurationInterface
                         ->defaultValue('single')
                         ->validate()
                         ->ifNotInArray($supportedInheritanceTypes)
-                        ->thenInvalid('The %s inheritance type is not supported. Please choose one of '
-                                    .json_encode($supportedInheritanceTypes))
+                        ->thenInvalid(
+                            'The %s inheritance type is not supported. Please choose one of '
+                                .json_encode($supportedInheritanceTypes)
+                        )
                         ->end()
                     ->end()
                 ->end()
